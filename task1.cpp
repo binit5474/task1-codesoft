9918eafee0825c7a5d64dfadaea83bcf3d8e2970
binit5474 <115914613+binit5474@users.noreply.github.com>
2024-01-10 16:59:38 +0530

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,32 @@
+/*Create a program that generates a random number and asks the
+user to guess it. Provide feedback on whether the guess is too
+high or too low until the user guesses the correct number ? */
+
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+    srand(static_cast<unsigned int>(time(0))); 
+    int secretNumber = rand() % 100 + 1; 
+    int guess;
+    
+    std::cout << "Welcome to the Number Guessing Game!\n";
+    std::cout << "Try to guess the number between 1 and 100.\n";
+
+    do {
+        std::cout << "Enter your guess: ";
+        std::cin >> guess;
+
+        if (guess > secretNumber) {
+            std::cout << "Too high! Try again.\n";
+        } else if (guess < secretNumber) {
+            std::cout << "Too low! Try again.\n";
+        } else {
+            std::cout << "Congratulations! You guessed the number.\n";
+        }
+    } while (guess != secretNumber);S
+
+    return 0;
+}
